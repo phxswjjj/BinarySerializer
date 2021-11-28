bdcb9dbcaa45907474a280ac8ac8dfaf9d022b89
phxswjjj <phxswjjj@gmail.com>
2021-11-28 16:35:25 +0800

    init

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.txtSerialized = new System.Windows.Forms.TextBox();
+            this.btnSaveFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(162, 57);
+            this.btnSelectFile.TabIndex = 1;
+            this.btnSelectFile.Text = "Select File";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // txtSerialized
+            // 
+            this.txtSerialized.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSerialized.Location = new System.Drawing.Point(12, 75);
+            this.txtSerialized.Multiline = true;
+            this.txtSerialized.Name = "txtSerialized";
+            this.txtSerialized.Size = new System.Drawing.Size(776, 363);
+            this.txtSerialized.TabIndex = 2;
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.Location = new System.Drawing.Point(626, 12);
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(162, 57);
+            this.btnSaveFile.TabIndex = 1;
+            this.btnSaveFile.Text = "Save File";
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.btnSaveFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSerialized);
+            this.Controls.Add(this.btnSaveFile);
+            this.Controls.Add(this.btnSelectFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox txtSerialized;
+        private System.Windows.Forms.Button btnSaveFile;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            var fileSelector = new OpenFileDialog();
+            fileSelector.InitialDirectory = Application.StartupPath;
+            if (fileSelector.ShowDialog() == DialogResult.OK)
+            {
+                var filePath = fileSelector.FileName;
+                if (string.IsNullOrEmpty(filePath))
+                {
+                    MessageBox.Show("no file path");
+                    return;
+                }
+                using (var fr = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    var content = new byte[fr.Length];
+                    fr.Read(content, 0, (int)fr.Length);
+                    var base64 = Convert.ToBase64String(content);
+                    txtSerialized.Text = base64;
+                }
+            }
+        }
+
+        private void btnSaveFile_Click(object sender, EventArgs e)
+        {
+            var base64 = txtSerialized.Text.Trim();
+            if (string.IsNullOrEmpty(base64))
+                return;
+
+            var fileSelector = new SaveFileDialog();
+            fileSelector.InitialDirectory = Application.StartupPath;
+            if (fileSelector.ShowDialog() == DialogResult.OK)
+            {
+                var filePath = fileSelector.FileName;
+                if (string.IsNullOrEmpty(filePath))
+                {
+                    MessageBox.Show("no file path");
+                    return;
+                }
+                var content = Convert.FromBase64String(base64);
+                using (var fw = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    fw.Write(content, 0, content.Length);
+                }
+            }
+        }
+    }
+}
